c9eec829f38830cf0a0bc243f90ba0da046615d2
camelcdr <camel.cdr@gmail.com>
2020-07-18 00:43:00 +0800

    design page

---
+++ src/main/java/com/camel/tacos/Taco.java
@@ -0,0 +1,17 @@
+package com.camel.tacos;
+
+/**
+ * @author camel
+ * @date 2020/07/05 22:35
+ **/
+public class Taco {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/camel/tacos/TacoCloudApplication.java
+++ src/main/java/com/camel/tacos/TacoCloudApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TacoCloudApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        System.out.println(TacoCloudApplication.class.getResource("/").toString());
         SpringApplication.run(TacoCloudApplication.class, args);
     }
 
